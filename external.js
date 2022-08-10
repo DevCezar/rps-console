3d09fdd7b18127a013859cee3fd14bc066b2f18b
Cesar <rCesar0251@gmail.com>
2022-08-09 23:27:55 -0500

    Add Winner dialogue

--- external.js
+++ external.js
@@ -1,5 +1,7 @@
 let computerString; 
 let userString;
+var compScore = 0;
+var userScore = 0;
 
 // RPS Function
 function computerChoice() {
@@ -15,7 +17,7 @@ function computerChoice() {
 
 // Question user, return answer
 function question() {
-    let answer = prompt("Rock, Paper, Scissors... SHOOT!");
+    let answer = prompt("Rock, Paper, Scissors?");
     let caseSens = answer.toLowerCase();
     if (caseSens == 'scissors') {
         userString = 'scissors';
@@ -24,32 +26,57 @@ function question() {
     } else if (caseSens == 'paper'){
         userString = 'paper';
     } else {
-        console.log('invalid choice, choose Rock, Paper, Scissors');
+        console.log('choose Rock, Paper, Scissors');
     }
 }
 
 //scenarios
 function playRound(userString, computerString) {
     if (userString === "rock" && computerString === "paper") {
+        compScore++;
         return (`Computer wins :( ${computerString} beats ${userString}`);
     } else if (userString === "paper" && computerString === "scissors") {
+        compScore++;
         return (`Computer wins :( ${computerString} beats ${userString}`);
     } else if (userString === "scissors" && computerString === "rock") {
+        compScore++;
         return (`Computer wins :( ${computerString} beats ${userString}`);
     } else if (userString === "rock" && computerString === "scissors") {
+        userScore++;
         return (`You win! ${userString} beats ${computerString}`);
     } else if (userString === "paper" && computerString === "rock") {
+        userScore++;
         return (`You win! ${userString} beats ${computerString}`);
     } else if (userString === "scissors" && computerString === "paper") {
+        userScore++;
         return (`You win! ${userString} beats ${computerString}`);
-    } else if (userString === computerString ) {
-        return (`Tie, ${computerString} and ${userString} are equal`);
     } else {
-        return 'try again'
+        return (`Tie, ${computerString} and ${userString} are equal`);
+    } 
+}
+
+// winner 
+function winners(userScore, compScore) {
+    if (userScore < compScore) {
+        console.log('You lose, \nbetter luck next time');
+    } else if (userScore > compScore) {
+        console.log('Winner winner chicken dinner!');
+    } else {
+        console.log('Tie game');
     }
 }
 
-computerChoice();
-question()
-console.log(playRound(userString, computerString));
-playRound();
+//actual game
+function game() { 
+    playRound(userString, computerString);
+    for (let i = 1; i < 6; i++) {
+        computerChoice();
+        question();
+        console.log('Round ' + i + ' ' + playRound(userString, computerString));
+        console.log(`your score: ${userScore} computer score ${compScore}`);
+    }
+    return winners(userScore, compScore);
+}
+
+
+game();
